ce8d51e8e87bc478a1680ed1a31a766a0de6ec3d
j-dolenc <jd5436@student.uni-lj.si>
2019-12-04 09:59:23 +0100

    day 4, skipped day 3 and modified day 2

--- Naloga2.java
+++ Naloga2.java
@@ -1,6 +1,3 @@
-import java.util.*;
-
-import javax.annotation.PostConstruct;
 
 import java.io.*;
 public class Naloga2{
@@ -24,19 +21,12 @@ public class Naloga2{
         for(int i = 0; i < tabela.length; i++){
             stevilke[i] = Integer.parseInt(tabela[i]);
         }
-        //writer.write(Arrays.toString(stevilke));
+
 
         int dolzina = stevilke.length;
 
-        //stevilke[1] = 12;
-        //stevilke[2] = 2;
-       
         int[] rezerva = new int[dolzina];
         rezerva = stevilke;
-        
-        
-        
-        
         for(int j = 0; j < 100; j++){
             //stevilke[1] = j;
             for(int k = 0; k < 100; k++){
@@ -47,16 +37,11 @@ public class Naloga2{
                 rezerva[1] = j;
                 rezerva[2] = k;
                 //stevilke[2] = k;
-                
                 for(int i = 0; i <dolzina; i= i +4 ){
-                    
                     int posZero = rezerva[i];
                     int posOne = rezerva[i + 1];
                     int posTwo = rezerva[i + 2];
                     int posTri = rezerva[i + 3];
-                    
-                    
-                    
                     if(posZero == 1){
                         rezerva[posTri] = rezerva[posOne] + rezerva[posTwo];
                     }
@@ -64,25 +49,14 @@ public class Naloga2{
                         rezerva[posTri] = rezerva[posOne] * rezerva[posTwo];
                     }
                     else{
-                        
                         break;
-                    }
-                  
+                    }     
                 }
                 if(rezerva[0] == 19690720){
                     System.out.println(j + " "  + k + " " + stevilke[0]);
                 }
             }
-            
         }
-
-        
-
-        
-
-
-        
-
         reader.close();
         writer.close();
     }
---
+++ n4.java
@@ -0,0 +1,54 @@
+
+
+
+public class n4{
+    public static void main(String[] args){
+        
+        int spMeja = 165432;
+        int zgMeja = 707912;
+
+
+        int koliko= 0;
+        
+        for(int i = spMeja; i <= zgMeja; i++){
+            boolean seVeca = true;
+            int num =i;
+            int prev = 10;
+            while (num!=0) { 
+                int rem = num % 10; 
+                num /= 10; 
+                if (rem > prev){
+                    seVeca = false;
+                    break;
+                } 
+            
+                prev = rem; 
+            } 
+            num = i;
+            boolean imaDvaZaporedna = false;
+            
+            int stPonovitev = 1;
+            if(seVeca){
+                while (num!=0) { 
+                    int rem = num % 10; 
+                    num /= 10; 
+                    if (rem == prev){
+                        stPonovitev++;
+                        if(stPonovitev == 2 && num%10 != rem){
+                            imaDvaZaporedna = true;
+                        }
+                    } 
+                    else{
+                        stPonovitev = 1;
+                    }
+                
+                    prev = rem; 
+                }
+            }
+            if(imaDvaZaporedna && seVeca){
+                koliko++;
+            }
+        }
+        System.out.println(koliko);
+    }
+}
