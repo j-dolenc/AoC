af91ac85a3784f030ea284ee86fddac79d3bec0e
j-dolenc <jurij1.dolenc@gmail.com>
2020-12-03 06:35:48 +0100

    part 1 and part 2 shown day 3

--- 2020/Aoc3.java
+++ 2020/Aoc3.java
@@ -11,14 +11,33 @@ public class Aoc3 {
                 grid[i][j] = line.charAt(j);
             }
         }
+        //part 1
+        int k = 0;
+        int h =0;
+        int drevesa = 0;
+        while(k != 323){
+            if(grid[k][h] == '#'){
+                drevesa++;
+            }
+            k+=1;
+            h+=3;
+            if(h>= 31) h %= 31;
+        }
+        System.out.println(drevesa);
+
+
+        //part 2
         BigInteger drevesa1 = BigInteger.ZERO;
         BigInteger drevesa2 = BigInteger.ZERO;;
         BigInteger drevesa3 = BigInteger.ZERO;;
         BigInteger drevesa4 = BigInteger.ZERO;;
         BigInteger drevesa5 = BigInteger.ZERO;;
-        
-        int k = 0;
-        int h =0;
+
+
+
+
+        k = 0;
+        h =0;
         while(k != 323){
             if(grid[k][h] == '#'){
                 BigInteger test = new BigInteger("1");
@@ -73,7 +92,7 @@ public class Aoc3 {
             h+=1;
             if(h == 31) h = 0;
         }
-        System.out.println(drevesa1 +" "+drevesa2 +" "+drevesa3 +" "+drevesa4 +" "+drevesa5 +" ");
+        //System.out.println(drevesa1 +" "+drevesa2 +" "+drevesa3 +" "+drevesa4 +" "+drevesa5 +" ");
         String hm  =drevesa1.multiply(drevesa2.multiply(drevesa3.multiply(drevesa4.multiply(drevesa5)))).toString();
         System.out.println(hm);    
         reader.close();
