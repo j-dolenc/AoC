02571820f65268a2c4724a25c088edc52681e44d
Jurij Dolenc <44226726+j-dolenc@users.noreply.github.com>
2022-12-04 09:34:24 +0000

    fix day4

--- 2022/day4/aoc41.py
+++ 2022/day4/aoc41.py
@@ -1,16 +1,14 @@
+
 import re
 
 with open("input.txt", encoding='utf8') as f:
     grid = []
     reg = ",|-"
     sum=0
+
     for line in f:
         curr = [int(i) for i in re.split(reg, line.strip())]
-        grid.append([int(i) for i in re.split(reg, line.strip())])
-        part1 = range(curr[0],curr[1]+1)
-        part2 = range(curr[2],curr[3]+1)
-        
-        if(set(part1).intersection(set(part2))):
+        if (curr[0] >= curr[2] and curr[1]<=curr[3]) or (curr[0] <= curr[2] and curr[1]>=curr[3]):
             sum+=1
 
     print(sum)
--- 2022/day4/aoc42.py
+++ 2022/day4/aoc42.py
@@ -4,11 +4,14 @@ with open("input.txt", encoding='utf8') as f:
     grid = []
     reg = ",|-"
     sum=0
-
     for line in f:
         curr = [int(i) for i in re.split(reg, line.strip())]
         grid.append([int(i) for i in re.split(reg, line.strip())])
-        if (curr[0] >= curr[2] and curr[1]<=curr[3]) or (curr[0] <= curr[2] and curr[1]>=curr[3]):
+        part1 = range(curr[0],curr[1]+1)
+        part2 = range(curr[2],curr[3]+1)
+        
+        if(set(part1).intersection(set(part2))):
             sum+=1
 
     print(sum)
+
