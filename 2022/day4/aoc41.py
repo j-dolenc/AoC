9d5e43003f326e454cc2f0e33b6f2a59b23cb069
Jurij Dolenc <44226726+j-dolenc@users.noreply.github.com>
2022-12-04 09:31:20 +0000

    day 4

---
+++ 2022/day4/aoc41.py
@@ -0,0 +1,16 @@
+import re
+
+with open("input.txt", encoding='utf8') as f:
+    grid = []
+    reg = ",|-"
+    sum=0
+    for line in f:
+        curr = [int(i) for i in re.split(reg, line.strip())]
+        grid.append([int(i) for i in re.split(reg, line.strip())])
+        part1 = range(curr[0],curr[1]+1)
+        part2 = range(curr[2],curr[3]+1)
+        
+        if(set(part1).intersection(set(part2))):
+            sum+=1
+
+    print(sum)
