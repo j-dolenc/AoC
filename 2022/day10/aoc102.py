c59ca423f8c0900727699fd4d67485cf5d92503e
jurijdolencuvc <jurij.dolenc@uvc-solutions.com>
2022-12-10 15:06:22 +0100

    day 10 compact

--- 2022/day10/aoc102.py
+++ 2022/day10/aoc102.py
@@ -1,29 +1,18 @@
 lines = open("input.txt", encoding="utf8")
-
 cycle = 1
 signalPower = 0
 register = 1
 pixel = 0
 for line in lines:
-
     command = [i for i in line.strip().split()]
-
-    if(command[0] == "noop"):
-        goFor = 1
-    else:
-        goFor = 2
+    if(command[0] == "noop"): goFor = 1
+    else: goFor = 2
     for i in range(0,goFor):
-        if(pixel == 40):
-            pixel = 0
-        if(abs(register-pixel) <= 1):
-            print("##", end = '')
-        else:
-            print("..", end='')
-        if(cycle%40 == 0):
-            print("")
+        if(pixel == 40): pixel = 0
+        if(abs(register-pixel) <= 1): print("##", end = '')
+        else: print("..", end='')
+        if(cycle%40 == 0): print("")
         pixel+=1
-        if(cycle%40 == 20):
-            signalPower += (cycle* register)
+        if(cycle%40 == 20): signalPower += (cycle* register)
         cycle+=1
-    if(command[0] != "noop"):
-        register+=int(command[1])
+    if(command[0] != "noop"): register+=int(command[1])
